5c252d7ce9f8539b4956c90a5d11fc4e13d02fc3
Vijitha bandara <94214061+VijithaBandara@users.noreply.github.com>
2024-02-16 10:56:51 +0530

    add title logo

--- BuySite/index.php
+++ BuySite/index.php
@@ -45,6 +45,9 @@ $conn->close();
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
+	
+    <!-- title logo -->
+    <link rel="icon" href="https://i.ibb.co/4mjBygC/1.png" type="image/x-icon">
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
