965da7642eb75e961dbce6278230e3e139786c3b
Vijitha bandara <94214061+VijithaBandara@users.noreply.github.com>
2024-02-16 11:04:24 +0530

    add title logo

--- inc/customerNavbar.php
+++ inc/customerNavbar.php
@@ -27,6 +27,8 @@ if ($result && $result->num_rows > 0) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- title logo -->
+    <link rel="icon" href="https://i.ibb.co/4mjBygC/1.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/font-awesome-4.7.0/css/font-awesome.min.css">
